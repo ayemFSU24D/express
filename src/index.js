7eb0e77e0dce5190c23572260ce251abc6fa53b4
Aylin <ayemfsu24@gmail.com>
2026-01-14 21:29:23 +0100

    test my routes without auth

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ app.get('/api/users/:id', (_req, res) => {
   res.json({ id: _req.params.id })
 })
 
-app.get('/api/posts/:postId/comments/:commentId', (_req, res) => {
-  res.json({ postId: _req.params.postId, commentId: _req.params.commentId })
-})
+app.use("/drug", drugRoutes);
+//app.use("/auth/drug", drugRoutes);
+app.use("/free/drug", drugRoutes);
 
 export default app
