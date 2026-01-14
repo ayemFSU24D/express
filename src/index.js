fe93b3f39e4be90afd67f8b789319bdbc75af522
Aylin <ayemfsu24@gmail.com>
2026-01-14 21:32:57 +0100

    without my routes

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ app.get('/api/users/:id', (_req, res) => {
   res.json({ id: _req.params.id })
 })
 
-app.use("/drug", drugRoutes);
+//app.use("/drug", drugRoutes);
 //app.use("/auth/drug", drugRoutes);
-app.use("/free/drug", drugRoutes);
+//app.use("/free/drug", drugRoutes);
 
 export default app
