b32f452e936757e702e5734556f0015638faac0e
Aylin <ayemfsu24@gmail.com>
2026-01-14 21:49:57 +0100

    test again only app.get("/"

--- src/Controllers/drugController.js
+++ src/Controllers/drugController.js
@@ -22,6 +22,24 @@ export const getDrugOrgans = async (req, res) => {
 };
 
 
-export const getDrugList = async (req, res) => { res.send("Backend running on Vercel ✅");
+export const getDrugList = async (req, res) => {
+  console.log("getDrugList körs");
+  try {
+    const drugs = await drugDB
+      .collection("clinicaltargets") // ← anpassa till din collections namn
+      .find({}, { projection: { _id: 0, generic_name: 1 } })
+      .toArray();
+
+    // Gör om till enkel lista
+    const drugNames = drugs
+      .map(d => d.generic_name)
+      .filter(Boolean)
+      .sort();
+
+    res.json(drugNames);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
 };
 
