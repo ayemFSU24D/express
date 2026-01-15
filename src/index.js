27ab6ba121241b6fcbe52df38449f92d5dc8d146
Aylin <ayemfsu24@gmail.com>
2026-01-15 21:37:50 +0100

    //

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/DB.js";
 import drugRoutes from "./routes/drugRoutes.js";
-import { authenticate } from "./middleware/authenticate.js";
+//import { authenticate } from "./middleware/authenticate.js";
 import admin from "firebase-admin";
 import fs from "fs";
 
