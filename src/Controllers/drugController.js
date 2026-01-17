9e1cdc469a9638f439d1cd423b4b4586c18b470b
Aylin <ayemfsu24@gmail.com>
2026-01-17 18:53:59 +0100

    i missed getDrugOrgans in my code

--- src/Controllers/drugController.js
+++ src/Controllers/drugController.js
@@ -1,7 +1,25 @@
-//import { updateDrugOrgansCache } from "../services/drugService.js";
+import { updateDrugOrgansCache } from "../services/drugService.js";
 import { drugDB } from "../config/DB.js";
 
+export const getDrugOrgans = async (req, res) => {
+  const drugName = req.params.name;
 
+  try {
+    const cached = await drugDB
+      .collection("drug_organs_cache")
+      .findOne({ drug: drugName });
+
+    if (!cached) {
+      const organs = await updateDrugOrgansCache(drugName);
+      return res.json({ drug: drugName, organs });
+    }
+
+    res.json({ drug: drugName, organs: cached.organs });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+};
 
 
 export const getDrugList = async (req, res) => {
